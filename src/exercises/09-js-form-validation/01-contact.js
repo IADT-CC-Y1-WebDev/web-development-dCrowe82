aa7d9fa8d54dee93917c771ffab0e2adafef9bb2
dCrowe82 <n00254023@iadt.ie>
2026-04-20 12:32:54 +0100

    finished exercise 9 - 1

--- src/exercises/09-js-form-validation/01-contact.js
+++ src/exercises/09-js-form-validation/01-contact.js
@@ -2,8 +2,14 @@ let submitBtn = document.getElementById('submit_btn');
 let commentForm = document.getElementById('comment_form');
 
 let nameInput = document.getElementById("name");
+let categoryInput = document.getElementById('category');
+let experienceInput = document.getElementsByName('experience');
+let languagesInput = document.getElementsByName('languages[]');
 
-let nameError = "name_error";
+let nameError = document.getElementById('name_error');
+let categoryError = document.getElementById('category_error');
+let experienceError = document.getElementById('experience_error');
+let languagesError = document.getElementById('languages_error');
 
 let errors = [];
 
@@ -15,11 +21,10 @@ function addError(location, errorMsg) {
 }
 
 function showErrors() {
-    document.getElementById(nameError).innerHTML = errors[nameError] || "";
-
-    // Object.keys(errors).forEach((error)=> {
-    //     document.getElementById(error).innerHTML = errors[error] || "";
-    // })
+    nameError.innerHTML = errors["name_error"] || "";
+    categoryError.innerHTML = errors["category_error"] || "";
+    experienceError.innerHTML = errors["experience_error"] || "";
+    languagesError.innerHTML = errors["languages_error"] || "";
 
 }
 
@@ -29,13 +34,29 @@ function onSubmitForm(evt) {
     errors = [];
     
     const name = nameInput.value.trim();
-    if (!name) {
-        addError(nameError, "name required");
-    } 
-    else if (!/^[A-Za-z ]+$/.test(name)) {
-        addError(nameError, "name can only contain letters and spaces");
+    if (!name) {addError("name_error", "Name is required")} 
+    else if (!/^[A-Za-z ]+$/.test(name)) {addError("name_error", "name can only contain letters and spaces")}
+
+    if (categoryInput.value == "") {addError("category_error", "Category is required");}
+
+    let expSelected = false;
+    for (let i = 0; i !== experienceInput.length; i++) {
+        if (experienceInput[i].checked) {
+            expSelected = true;
+            break;
+        }
     }
+    if (!expSelected) {addError("experience_error", "Experience is required")}
     
+    let langSelected = false;
+    for (let i = 0; i !== languagesInput.length; i++) {
+        if (languagesInput[i].checked) {
+            langSelected = true;
+            break;
+        }
+    }
+    if (!langSelected) {addError("languages_error", "Language is required")}
+
     showErrors();
 
     if (Object.keys(errors).length == 0) {
